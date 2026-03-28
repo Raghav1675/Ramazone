4dd914d6805465676d25d4e83b2e06a9db65b8ce
Raghav1675 <165365016+Raghav1675@users.noreply.github.com>
2026-03-29 04:27:54 +0530

    Refactor productRoutes to use controller function

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,30 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 
-const getProducts = async (req, res) => {
-  try {
-    const { search, category } = req.query;
-
-    let query = "SELECT * FROM products WHERE 1=1";
-    let values = [];
-
-    if (search) {
-      values.push(`%${search}%`);
-      query += ` AND name ILIKE $${values.length}`;
-    }
-
-    if (category) {
-      values.push(category);
-      query += ` AND main_category = $${values.length}`;
-    }
-
-    const result = await pool.query(query, values);
-    res.json(result.rows);
-
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Server error" });
-  }
-};
+const { getProducts } = require("../controllers/productController");
 router.get("/", getProducts);
 module.exports = router;
