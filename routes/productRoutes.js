b793e95ce9e1e2cf3bb40ebb7f0dd873f3053efd
Raghav1675 <165365016+Raghav1675@users.noreply.github.com>
2026-03-29 10:46:03 +0530

    Add getProductById route to productRoutes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 
-const { getProducts } = require("../controllers/productController");
+const { getProducts, getProductById } = require("../controllers/productController");
+router.get("/search", (req, res) => res.redirect(`/api/products?search=${req.query.q}`)); // optional
 router.get("/", getProducts);
+router.get("/:id", getProductById);
 module.exports = router;
